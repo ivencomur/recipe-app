25d32d19f81cb6f71dde9a726885bceef7f98a27
ICM <ivancortes@hotmail.com>
2026-01-09 14:33:14 -0500

    Add multi-language support: Spanish, French, and Google Translate widget

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -1,12 +1,13 @@
-﻿# recipe_project/urls.py
+# recipe_project/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import login_view, logout_view, logout_success_view, about_me_view
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
 
     path('admin/', admin.site.urls),
 
@@ -26,4 +27,5 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
